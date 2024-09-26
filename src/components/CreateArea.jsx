84fac325910be474f72a39d832253e0fc5de9265
Chodipilli Ajay <115925992+chodipilliajay@users.noreply.github.com>
2024-09-27 00:03:11 +0530

    Add files via upload

---
+++ src/components/CreateArea.jsx
@@ -0,0 +1,80 @@
+import React, { useState } from "react";
+import AddIcon from '@mui/icons-material/Add';
+import Fab from '@mui/material/Fab';
+import Zoom from '@mui/material/Zoom';
+
+function CreateArea(props) {
+
+  const [isExpanded, setExpand] = useState(false)
+
+    function expand(){
+      setExpand(true)
+    }
+
+
+  const [note, setNote] = useState({
+    title: "",
+    content: ""
+  });
+  
+  
+
+  function handleChange(event) {
+    const { name, value } = event.target;
+
+
+
+
+    setNote(prevNote => {
+      return {
+        ...prevNote,
+        [name]: value
+      };
+    });
+  }
+
+  function submitNote(event) {
+    props.onAdd(note);
+    setNote({
+      title: "",
+      content: ""
+    });
+    event.preventDefault();
+  }
+
+  return (
+    <div>
+      <form className="create-note">
+        {isExpanded &&(        <input
+          name="title"
+          onChange={handleChange}
+          value={note.title}
+          placeholder="Title"
+        />
+
+        )}
+
+        <textarea
+          name="content"
+          onClick={expand}
+          onChange={handleChange}
+          value={note.content}
+          placeholder="Take a note..."
+          rows={isExpanded?3:1}
+        />
+        
+        {isExpanded&&(<Zoom in={true}><Fab onClick={submitNote}><AddIcon/></Fab></Zoom>)}
+        
+      </form>
+    </div>
+  );
+}
+
+export default CreateArea;
+
+
+
+
+// npm install @mui/material @emotion/react @emotion/styled
+
+// npm install @mui/icons-material
